522719a8c8209aff00c28215e22ede461fdd16d3
Jortz <johnnydortiz@gmail.com>
2021-08-17 09:24:28 -0400

    guessing game completed

---
+++ jonathan_ortiz_guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,33 @@
+class GuessingGame
+    def initialize(min, max)
+        @secret_num = rand(min..max)
+        @num_attempts = 0
+        @game_over = false
+    end
+
+    attr_reader :num_attempts
+
+    def game_over?
+        @game_over
+    end
+
+    def check_num(num)
+        @num_attempts += 1
+        if num == @secret_num
+            puts "you win"
+            @game_over = true
+        elsif num > @secret_num
+            puts "too big"
+        else
+            puts "too small"
+        end
+    end
+
+    def ask_user
+        puts "enter a number:"
+        self.check_num(gets.chomp.to_i)
+    end
+
+end
+
+
